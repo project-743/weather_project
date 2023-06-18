2d2f4a7e9fcc66ac416a6034023c3c951c01269b
Sahai Jordi Alan A <73820349+SJAlanA@users.noreply.github.com>
2023-06-18 13:04:42 +0530

    Experimenting with about jsx

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import './about_style.css';
 
 function About(){
-    <div>
-        {/* <footer id="footer">
-            <h1>ALAN</h1>
-            <i class="fa-brands fa-twitter CCicons"></i>
-            <i class="fa-brands fa-facebook-f CCicons"></i>
-            <i class="fa-brands fa-instagram CCicons"></i>
-            <i class="fa-solid fa-envelope CCicons"></i>
+    return (
+        <footer id="footer">
+            <h1>SkySaavy</h1>
+            <button class="fa-brands fa-twitter CCicons"></button>
+            <button class="fa-brands fa-facebook-f CCicons"></button>
+            <button class="fa-brands fa-instagram CCicons"></button>
+            <button class="fa-solid fa-envelope CCicons"></button>
             <p>© Copyright SkySaavy</p>
-        </footer> */}
-        <h1>HELLO</h1>
-    </div>
+        </footer>
+    );
 }
 
 export default About;
